5fba90f70f38b203c84f98353fcc3c974a994327
AndreyXXX228 <secondakk002@gmail.com>
2026-03-23 15:53:53 +0300

    промежуточный коммит

--- obj/Release/net9.0/Hackaton.AssemblyInfo.cs
+++ obj/Release/net9.0/Hackaton.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hackaton")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ab4f0845c558b62159a6c743e61eb1b8bced4b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hackaton")]
